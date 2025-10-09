71b113e02d60a326cab63c3acacd83b272b4e7a6
sai rushith <mithintisairushith@gmail.com>
2025-10-10 00:53:00 +0530

    added backenddd url

--- Server/index.js
+++ Server/index.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(cookieParser());    
 app.use(
 	cors({
-		origin:"https://codify-inky.vercel.app/",
+		origin:"https://codify-inky.vercel.app",
 		credentials:true,
 	})
 )
